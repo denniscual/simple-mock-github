f4a8baa59b43f102a102ac11444daefc27d19254
Dennis Cual <mamaylovepapay@outlook.com>
2020-09-09 00:43:35 +0800

    chore(home): restructuring the header

--- src/pages/Home/HeaderActions.tsx
+++ src/pages/Home/HeaderActions.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
 import { ButtonGroup, IconButton, Button, Headings } from '../../components'
 import { Link as RootLink } from 'react-router-dom'
 import Download from '../../components/icons/Download'
@@ -33,15 +32,6 @@ const RepoLink = S.styled(Link, {
     fontWeight: '$bold',
 })
 
-const Header = S.styled('header', {
-    px: '$8',
-    paddingTop: '$4',
-    backgroundColor: '$lightGray',
-    display: 'grid',
-    rowGap: '$4',
-})
-Header.displayName = 'Header'
-
 const HeaderAction = S.styled('div', {
     display: 'flex',
     justifyContent: 'space-between',
@@ -52,23 +42,20 @@ HeaderAction.displayName = 'HeaderAction'
 
 export default function HomeHeader() {
     return (
-        <Header>
-            <HeaderAction>
-                <LightH3>
-                    <RepoIcon>
-                        <Download />
-                    </RepoIcon>
-                    <Link to="#">denniscual</Link> <Separator>/</Separator>{' '}
-                    <RepoLink to="#">restatum</RepoLink>
-                </LightH3>
-                <ButtonGroup>
-                    <IconButton size="sm" startIcon={<Download />}>
-                        Unstar
-                    </IconButton>
-                    <Button size="sm">1</Button>
-                </ButtonGroup>
-            </HeaderAction>
-            <nav>Nav</nav>
-        </Header>
+        <HeaderAction>
+            <LightH3>
+                <RepoIcon>
+                    <Download />
+                </RepoIcon>
+                <Link to="#">denniscual</Link> <Separator>/</Separator>{' '}
+                <RepoLink to="#">restatum</RepoLink>
+            </LightH3>
+            <ButtonGroup>
+                <IconButton size="sm" startIcon={<Download />}>
+                    Unstar
+                </IconButton>
+                <Button size="sm">1</Button>
+            </ButtonGroup>
+        </HeaderAction>
     )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import HomeHeader from './HomeHeader'
+import HeaderActions from './HeaderActions'
+import S from '../../stitches.config'
+
+const Header = S.styled('header', {
+    px: '$8',
+    paddingTop: '$4',
+    backgroundColor: '$lightGray',
+    display: 'grid',
+    rowGap: '$4',
+})
+Header.displayName = 'Header'
 
 export default function Home() {
     return (
         <div>
-            <HomeHeader />
+            <Header>
+                <HeaderActions />
+                <nav>Nav</nav>
+            </Header>
             <main>
                 <Outlet />
             </main>
