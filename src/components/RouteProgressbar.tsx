23dbbd8a74d56ff5e4b13b56ac08b49511fc21a1
Dennis Cual <mamaylovepapay@outlook.com>
2020-10-04 01:17:41 +0800

    refactor: delete Progressbar and move the codes into RouteProgressbar

--- src/components/RouteProgressbar.tsx
+++ src/components/RouteProgressbar.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useLocationPending } from 'react-router-dom'
-import Progressbar from './Progressbar'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+NProgress.configure({ showSpinner: false })
 
 /*
  * A progress bar which mimics the progress bar of youtube, etc.
@@ -8,5 +11,21 @@ import Progressbar from './Progressbar'
  */
 export default function RouteProgressbar() {
     const pending = useLocationPending()
-    return <Progressbar pending={pending} />
+
+    React.useEffect(() => {
+        // Add timeout so that we can delay the showing of progress bar.
+        // We set 500 ms just like the delay for showing the Loader Component.
+        // We don't need to show the timeout if the routes rendering navigation
+        // only takes below 500ms. Show if the rendering/showing comes up above.
+        const timeoutId = setTimeout(() => {
+            if (pending) {
+                NProgress.start()
+            }
+        }, 500)
+        if (!pending) {
+            NProgress.done()
+        }
+        return () => clearTimeout(timeoutId)
+    }, [pending])
+    return null
 }
